e963e1993e3527d713deefaec8a19534deab8b40
nguyenkien0703 <nguyenkien07032003ns@gmail.com>
2023-12-12 01:50:19 +0700

    update

---
+++ PY02043.py
@@ -0,0 +1,11 @@
+#  dem so trong xau 
+for i in range(int(input())):
+    s, n = input(), input()
+    l1, l2 = len(s), len(n)
+    cnt = i = 0
+    while i <l1:
+        if s[i: i + l2] == n:
+            cnt +=1 
+            i += l2
+        else: i+=1
+    print(cnt)
